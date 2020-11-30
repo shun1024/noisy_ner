a402590be240e6b0893faa9951359b5415a6c155
sliao3 <shunliao0@gmail.com>
2020-11-29 21:21:22 -0500

    1

--- embedding/build_matching_embedding.py
+++ embedding/build_matching_embedding.py
@@ -24,15 +24,14 @@ if __name__ == "__main__":
     for infile in files:
         if 'txt' in infile:
             sentences += read_all_sentences(infile)
-
-    model = Word2Vec(sentences, size=300, min_count=1)
+    model = Word2Vec(size=300, min_count=1)
     model.build_vocab(sentences)
     training_examples_count = model.corpus_count
 
     glove_bin = '/h/sliao3/deid/noisy_ner/data/glove/glove.bin'
-    glove = KeyedVectors.load()
-    model.build_vocab([list(glove.keys())], update=True)
-    model.intersect_word2vec_format(glove_bin, binary=True, lockf=1.0)
+    glove = KeyedVectors.load(glove_bin)
+    model.build_vocab([list(glove.vocab.keys())], update=True)
+    model.intersect_word2vec_format('/h/sliao3/.flair/embeddings/glove.word2vec.txt', binary=False, lockf=1.0)
     model.train(sentences, total_examples=training_examples_count, epochs=model.iter)
     model.save(os.path.join(data_dir, 'glove.bin'))
 
